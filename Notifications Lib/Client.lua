abd0733ba165756d287ee40307576e63e00806c3
notr3th <notr3th@gmail.com>
2025-07-04 09:25:18 +0800

    Update Client.lua

--- Notifications Lib/Client.lua
+++ Notifications Lib/Client.lua
@@ -1,16 +1,22 @@
 local Notification = {}
 
 function Notification:Notify(Text, Details, Miscellaneous)
-    local CoreGui = game:GetService("CoreGui")  
+    local CoreGui = game:GetService("CoreGui")
+
+    local container = CoreGui:FindFirstChild("STX_Notification")
+    if not container then
+        container = Instance.new("ScreenGui")
+        container.Name = "STX_Notification"
+        container.ResetOnSpawn = false
+        container.IgnoreGuiInset = true
+        container.Parent = CoreGui
+    end
+
     local Type = string.lower(tostring(Details.Type))
+
     local Shadow = Instance.new("ImageLabel")
-    local Window = Instance.new("Frame")
-    local Outline = Instance.new("Frame")
-    local Title = Instance.new("TextLabel")
-    local Description = Instance.new("TextLabel")
-    
     Shadow.Name = "Shadow"
-    Shadow.Parent = CoreGui:FindFirstChild("STX_Notification")
+    Shadow.Parent = container
     Shadow.AnchorPoint = Vector2.new(0.5, 0.5)
     Shadow.BackgroundTransparency = 1
     Shadow.BorderSizePixel = 0
@@ -18,10 +24,11 @@ function Notification:Notify(Text, Details, Miscellaneous)
     Shadow.Size = UDim2.new(0, 0, 0, 0)
     Shadow.Image = "rbxassetid://1316045217"
     Shadow.ImageColor3 = Color3.fromRGB(0, 0, 0)
-    Shadow.ImageTransparency = 0.400
+    Shadow.ImageTransparency = 0.4
     Shadow.ScaleType = Enum.ScaleType.Slice
     Shadow.SliceCenter = Rect.new(10, 10, 118, 118)
-    
+
+    local Window = Instance.new("Frame")
     Window.Name = "Window"
     Window.Parent = Shadow
     Window.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
@@ -29,7 +36,8 @@ function Notification:Notify(Text, Details, Miscellaneous)
     Window.Position = UDim2.new(0, 5, 0, 5)
     Window.Size = UDim2.new(0, 230, 0, 80)
     Window.ZIndex = 2
-    
+
+    local Outline = Instance.new("Frame")
     Outline.Name = "Outline"
     Outline.Parent = Window
     Outline.BackgroundColor3 = Details.OutlineColor
@@ -37,12 +45,11 @@ function Notification:Notify(Text, Details, Miscellaneous)
     Outline.Position = UDim2.new(0, 0, 0, 25)
     Outline.Size = UDim2.new(0, 230, 0, 2)
     Outline.ZIndex = 5
-    
+
+    local Title = Instance.new("TextLabel")
     Title.Name = "Title"
     Title.Parent = Window
-    Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     Title.BackgroundTransparency = 1
-    Title.BorderColor3 = Color3.fromRGB(27, 42, 53)
     Title.BorderSizePixel = 0
     Title.Position = UDim2.new(0, 8, 0, 2)
     Title.Size = UDim2.new(0, 222, 0, 22)
@@ -52,12 +59,11 @@ function Notification:Notify(Text, Details, Miscellaneous)
     Title.TextColor3 = Color3.fromRGB(220, 220, 220)
     Title.TextSize = 12
     Title.TextXAlignment = Enum.TextXAlignment.Left
-    
+
+    local Description = Instance.new("TextLabel")
     Description.Name = "Description"
     Description.Parent = Window
-    Description.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     Description.BackgroundTransparency = 1
-    Description.BorderColor3 = Color3.fromRGB(27, 42, 53)
     Description.BorderSizePixel = 0
     Description.Position = UDim2.new(0, 8, 0, 34)
     Description.Size = UDim2.new(0, 216, 0, 40)
@@ -70,30 +76,23 @@ function Notification:Notify(Text, Details, Miscellaneous)
     Description.TextXAlignment = Enum.TextXAlignment.Left
     Description.TextYAlignment = Enum.TextYAlignment.Top
 
-    if Type == "Default" then
-
-            local script = Instance.new("LocalScript", Shadow)
-
+    if Type == "default" then
+        coroutine.wrap(function()
             Shadow:TweenSize(UDim2.new(0, 240, 0, 90), "Out", "Linear", 0.2)
             Window.Size = UDim2.new(0, 230, 0, 80)
             Outline:TweenSize(UDim2.new(0, 0, 0, 2), "Out", "Linear", Details.Time)
-    
             wait(Details.Time)
-        
             Shadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
-
             wait(0.2)
-
             Shadow:Destroy()
-
-    elseif Type == "Image" then
+        end)()
+    elseif Type == "image" then
         Shadow:TweenSize(UDim2.new(0, 240, 0, 90), "Out", "Linear", 0.2)
         Window.Size = UDim2.new(0, 230, 0, 80)
         Title.Position = UDim2.new(0, 24, 0, 2)
 
         local ImageButton = Instance.new("ImageButton")
         ImageButton.Parent = Window
-        ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
         ImageButton.BackgroundTransparency = 1
         ImageButton.BorderSizePixel = 0
         ImageButton.Position = UDim2.new(0, 4, 0, 4)
@@ -103,28 +102,20 @@ function Notification:Notify(Text, Details, Miscellaneous)
         ImageButton.Image = Miscellaneous.Image
         ImageButton.ImageColor3 = Miscellaneous.ImageColor
 
-
-            local script = Instance.new('LocalScript', Shadow)
+        coroutine.wrap(function()
             Outline:TweenSize(UDim2.new(0, 0, 0, 2), "Out", "Linear", Details.Time)
-
             wait(Details.Time)
-
             Shadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
-            
             wait(0.2)
-
             Shadow:Destroy()
-
-    elseif Type == "Option" then
+        end)()
+    elseif Type == "option" then
         Shadow:TweenSize(UDim2.new(0, 240, 0, 110), "Out", "Linear", 0.2)
         Window.Size = UDim2.new(0, 230, 0, 100)
 
         local Accept = Instance.new("ImageButton")
-        local Decline = Instance.new("ImageButton")
-        
         Accept.Name = "Accept"
         Accept.Parent = Window
-        Accept.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
         Accept.BackgroundTransparency = 1
         Accept.BorderSizePixel = 0
         Accept.Position = UDim2.new(0, 28, 0, 76)
@@ -134,9 +125,9 @@ function Notification:Notify(Text, Details, Miscellaneous)
         Accept.Image = "http://www.roblox.com/asset/?id=6031094667"
         Accept.ImageColor3 = Color3.fromRGB(83, 230, 50)
 
+        local Decline = Instance.new("ImageButton")
         Decline.Name = "Decline"
         Decline.Parent = Window
-        Decline.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
         Decline.BackgroundTransparency = 1
         Decline.BorderSizePixel = 0
         Decline.Position = UDim2.new(0, 7, 0, 76)
@@ -146,46 +137,38 @@ function Notification:Notify(Text, Details, Miscellaneous)
         Decline.Image = "http://www.roblox.com/asset/?id=6031094678"
         Decline.ImageColor3 = Color3.fromRGB(255, 84, 84)
 
-
-            local script = Instance.new("LocalScript", Shadow)
-            local Pending = true
+        coroutine.wrap(function()
+            local active = true
 
             Accept.MouseButton1Click:Connect(function()
                 pcall(function()
                     Miscellaneous.Callback(true)
                 end)
+                active = false
                 Shadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
-                
                 wait(0.2)
-
                 Shadow:Destroy()
-                Pending = false
             end)
 
             Decline.MouseButton1Click:Connect(function()
                 pcall(function()
                     Miscellaneous.Callback(false)
                 end)
+                active = false
                 Shadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
-                
                 wait(0.2)
-
                 Shadow:Destroy()
-                Pending = false
             end)
-            
+
             Outline:TweenSize(UDim2.new(0, 0, 0, 2), "Out", "Linear", Details.Time)
-    
             wait(Details.Time)
 
-            if Pending == true then
+            if active then
                 Shadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
-                
                 wait(0.2)
-
                 Shadow:Destroy()
             end
-
+        end)()
     end
 end
 
